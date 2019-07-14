9f18304a2ac833af519ed509f4e4678f79c83cd3
Alex G. Wolff <alexgwolff@outlook.com>
2019-07-14 16:04:10 -0300

    eslint fix

--- struct-core/rollup.config.dev.js
+++ struct-core/rollup.config.dev.js
@@ -1,29 +1,29 @@
-import html from "rollup-plugin-html";
-import { eslint } from "rollup-plugin-eslint";
-import acornStaticClassFeatures from "acorn-static-class-features";
-import acornPrivateMethods from "acorn-private-methods";
-import acornPrivateClassElements from "acorn-private-class-elements";
-import acornClassFields from "acorn-class-fields";
+import html from 'rollup-plugin-html';
+import { eslint } from 'rollup-plugin-eslint';
+import acornStaticClassFeatures from 'acorn-static-class-features';
+import acornPrivateMethods from 'acorn-private-methods';
+import acornPrivateClassElements from 'acorn-private-class-elements';
+import acornClassFields from 'acorn-class-fields';
 
 export default {
   acorn: {
-    ecmaVersion: "10"
+    ecmaVersion: '10',
   },
   acornInjectPlugins: [
     acornClassFields,
     acornStaticClassFeatures,
     acornPrivateMethods,
-    acornPrivateClassElements
+    acornPrivateClassElements,
   ],
-  input: "src/main.js",
+  input: 'src/main.js',
   output: {
-    file: "dist/bundle.js",
-    format: "esm"
+    file: 'dist/bundle.js',
+    format: 'esm',
   },
   plugins: [
     html({
-      include: "**/*.html"
+      include: '**/*.html',
     }),
-    eslint(), 
-  ]
+    eslint(),
+  ],
 };
